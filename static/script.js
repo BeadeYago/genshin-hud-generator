397c298fb44b933f585c1916e3ee37bc11492400
BeadeYago <yagito2001@gmail.com>
2025-02-20 12:35:40 -0300

    feat: new detailed UI

--- static/script.js
+++ static/script.js
@@ -2,65 +2,59 @@ document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.getElementById("miCanvas");
     const ctx = canvas.getContext("2d");
     const canvasDownload = document.getElementById("canvasDownload")
+
     let nameInput = document.getElementById("nameInput")
     let jobInput = document.getElementById("jobInput")
     let dialogInput = document.getElementById("dialogInput")
     let automaticInput = document.getElementById("automaticInput") 
-    let hasJobInput = document.getElementById("hasJob")
-    let TextLenghtInput = document.getElementById("TextLenghtInput")
+    const lengthSlider = document.getElementById('lengthSlider');
+    const spaceSlider = document.getElementById('spaceSlider');
+    const removeSpaceCheckbox = document.getElementById('removeSpaceCheckbox');
 
+    let lineImage = new Image()
     let blackFade = new Image();
-    blackFade.src = '../static/content/images/backfade.png';
-
     let automaticHud = new Image();
-    automaticHud.src = '../static/content/images/automatic.png';
-
     let dialogLines = new Image();
+    let startImageLeft = new Image();
+    let endImageLeft = new Image();
+    let startImageRight = new Image();
+    let endImageRight = new Image();
+
+
+    //Starts from Outside ends in Middle
+    lineImage.src = '../static/content/images/line.png';
+    startImageLeft.src = '../static/content/images/startLeftLine.png';
+    endImageLeft.src = '../static/content/images/endLeftLine.png';
+    startImageRight.src = '../static/content/images/startRightLine.png';
+    endImageRight.src = '../static/content/images/endRightLine.png';
+    blackFade.src = '../static/content/images/backfade.png';
+    automaticHud.src = '../static/content/images/automatic.png';
     dialogLines.src = '../static/content/images/dialoglines/longJob.png'
 
     dialogLines.onload = function(){
         drawCanvas();
     };
 
-    nameInput.addEventListener("input", function() {
-        drawCanvas();
-    });
+    nameInput.addEventListener("input", drawCanvas);
 
-    jobInput.addEventListener("input", function() {
-        drawCanvas();
-    });
+    jobInput.addEventListener("input", drawCanvas);
 
-    dialogInput.addEventListener("input", function() {
-        drawCanvas();
-    });
+    dialogInput.addEventListener("input", drawCanvas);
 
-    automaticInput.addEventListener("input", function() {
-        drawCanvas();
-    });
+    automaticInput.addEventListener("input", drawCanvas);
 
-    hasJobInput.addEventListener("click", function() {
-        if(hasJob.checked){
-                dialogLines.src = '../static/content/images/dialoglines/longJob.png'
-            } else{
-                dialogLines.src = '../static/content/images/dialoglines/long.png';
-            }
-            drawCanvas()
-    })
-
-    TextLenghtInput.addEventListener("change", function(){
-        if(TextLenghtInput.value == "short" && hasJob.checked){
-            dialogLines.src = '../static/content/images/dialoglines/shortJob.png'
-        }
-            drawCanvas
-    })
+    lengthSlider.addEventListener('input', drawCanvas);
+    spaceSlider.addEventListener('input', drawCanvas);
+    removeSpaceCheckbox.addEventListener('change', drawCanvas);
 
     function drawCanvas() {
+        console.log("Canvas Updated")
         ctx.clearRect(0,0, canvas.width, canvas.height);
 
         //Draws Black Fades Behind
         ctx.drawImage(blackFade, 0, 0, canvas.width, canvas.height);
         ctx.drawImage(automaticHud, 0, 0, canvas.width, canvas.height);
-        ctx.drawImage(dialogLines, 0, 0, canvas.width, canvas.height);
+    //    ctx.drawImage(dialogLines, 0, 0, canvas.width, canvas.height);
 
         const lines = dialogInput.value.split('\n');
 
@@ -84,14 +78,62 @@ document.addEventListener("DOMContentLoaded", function() {
             ctx.fillText(line, 641, 640 + index * 30);
         })
 
-
-        // Dynamic Automatic (Still not)
+        // Dynamic Automatic
         ctx.fillStyle = "white"
         ctx.textAlign = "left"
         ctx.font = "15px ja-jp"
         ctx.fillText(automaticInput.value, 70, 37);
+
+              // Get the values from sliders
+      const lineLength = parseInt(lengthSlider.value);
+      const spaceBetween = parseInt(spaceSlider.value);
+
+      // Disable space slider when checkbox is checked
+      spaceSlider.disabled = removeSpaceCheckbox.checked;
+      jobInput.disabled = removeSpaceCheckbox.checked;
+
+
+      // Calculate the positions of the lines
+      const line1StartX = canvas.width / 2 - (removeSpaceCheckbox.checked ? 0 : spaceBetween / 2) - lineLength;
+      const line1EndX = canvas.width / 2 - (removeSpaceCheckbox.checked ? 0 : spaceBetween / 2);
+
+      const line2StartX = canvas.width / 2 + (removeSpaceCheckbox.checked ? 0 : spaceBetween / 2);
+      const line2EndX = canvas.width / 2 + (removeSpaceCheckbox.checked ? 0 : spaceBetween / 2) + lineLength;
+
+      // Set color for the first line (red)
+      ctx.strokeStyle = '#ffc701';
+      // Draw the first line
+      ctx.drawImage(lineImage, line1StartX, 1216 / 2 - lineImage.height / 2, lineLength, lineImage.height)
+ 
+
+      // Draw start image on the first line if checkbox is not checked
+      if (!removeSpaceCheckbox.checked) {
+        ctx.drawImage(startImageLeft, line1StartX - startImageLeft.width / 1, 1216 / 2 - startImageLeft.height / 2);
+        ctx.drawImage(endImageLeft, line1EndX - endImageLeft.width / 300, 1214 / 2 - endImageLeft.height / 2.2);
+      }
+
+      if(removeSpaceCheckbox.checked){
+        ctx.drawImage(startImageLeft, line1StartX - startImageLeft.width / 1, 1216 / 2 - startImageLeft.height / 2);
+        ctx.drawImage(startImageRight, line2EndX - startImageRight.width / 20, 1216 / 2 - startImageRight.height / 2);
+
+      }
+
+      // Set color for the second line (blue)
+      ctx.strokeStyle = '#ffc701';
+      // Draw the second line
+      ctx.drawImage(lineImage, line2StartX, 1216 / 2 - lineImage.height /2, lineLength, lineImage.height)
+
+
+      // Draw start image on the second line if checkbox is not checked
+      if (!removeSpaceCheckbox.checked) {
+        ctx.drawImage(endImageRight, line2StartX - endImageRight.width / 1, 1216 / 2 - endImageRight.height / 2);
+        ctx.drawImage(startImageRight, line2EndX - startImageRight.width / 20, 1216 / 2 - startImageRight.height / 2);
+      }
     }
 
+    drawCanvas();
+
+    //Download Canvas as PNG image
     canvasDownload.addEventListener('click', (e) =>{
         const pngDataUrl = canvas.toDataURL("image/png")
         canvasDownload.href = pngDataUrl;
